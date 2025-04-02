eb53667d3bb625e65bee62cb4e46ff702d359ebb
ebrandwein21 <150644846+ebrandwein21@users.noreply.github.com>
2025-04-02 16:35:49 -0500

    polished up csv parser

--- MyLittleMozart/src/edu/mu/csv/parsing/MidiCsvParcer.java
+++ MyLittleMozart/src/edu/mu/csv/parsing/MidiCsvParcer.java
@@ -9,20 +9,34 @@ import java.util.List;
 
 public class MidiCsvParcer{
 	
-	String file = "/Users/EthanBrandwein/Desktop/mystery_song.csv";
 	
-	List <MidiEventData> MidiEventDataList = new ArrayList<>();
+	List<MidiEventData> midiEventDataList = new ArrayList<>(); //what is this syntax error 
 	
-    
-	try {
-	BufferedReader csvReader = new BufferedReader(new FileReader(file));
-	String data; 
+    public void test( ) {
+	try(BufferedReader csvReader = new BufferedReader(new FileReader("/Users/EthanBrandwein/Desktop/mystery_song.csv"))) 
+	{
+	String data=""; 
 	
 	while((data = csvReader.readLine()) != null) {
-		System.out.println(data);
 		String[] objects = data.split(",");
-		System.out.println("start end trick" + objects[0] + "velocity" + objects[1] + "note" + objects[2] + "channel" + objects[3] + "instrument" + objects[4] + "note on off" + objects[5]);
+				
+		int startEndTick = Integer.parseInt(objects[0]);
+        int velocity = Integer.parseInt(objects[1]);
+        int note = Integer.parseInt(objects[2]);
+        int channel = Integer.parseInt(objects[3]);
+        int instrument = Integer.parseInt(objects[4]);
+        int noteOnOff = Integer.parseInt(objects[5]);
+
 		
+		MidiEventData midiEventData = new MidiEventData(startEndTick, velocity, note, channel, instrument, noteOnOff);
+		
+		//need to create instances where you would use the class and parse data from the constructor and add 
+		midiEventDataList.add(midiEventData);
+		
+		//test but get rid of after
+		System.out.println("start end trick" + objects[0] + "velocity" + objects[1] + "note" + objects[2] + "channel" + objects[3] + "instrument" + objects[4] + "note on off" + objects[5]);
+	    
+	
 	}
 	
 	} catch(FileNotFoundException e) {
@@ -31,8 +45,10 @@ public class MidiCsvParcer{
 	  catch(IOException error) {
 		  error.printStackTrace();
     }
-	return MidiEventDataList;
+	//return MidiEventDataList;
+	//create a folder inside the folder with the csv 
 }
 
 
 }
+
