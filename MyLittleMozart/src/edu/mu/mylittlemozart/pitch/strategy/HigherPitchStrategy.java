a7bb3f2663e3b79605473de51cd710a4214e411a
andergato <145510193+andergato@users.noreply.github.com>
2025-04-02 09:34:08 -0500

    Implemented Pitch Strategies with javadocs

--- MyLittleMozart/src/edu/mu/mylittlemozart/pitch/strategy/HigherPitchStrategy.java
+++ MyLittleMozart/src/edu/mu/mylittlemozart/pitch/strategy/HigherPitchStrategy.java
@@ -1,5 +1,14 @@
 package edu.mu.mylittlemozart.pitch.strategy;
 
-public class HigherPitchStrategy {
-
+public class HigherPitchStrategy implements PitchStrategy{
+	
+	/*
+	 * In the HigherPitchStrategy, the note integer input will be increased by two and returned.
+	 */
+	@Override
+	public int modifyPitch(int note) {
+		note = note + 2;
+		System.out.println("Increased pitch by two semitones. Note is now " + note + ".");
+		return note;
+	}
 }
