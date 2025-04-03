b4df358d4cab48b1b0c42e7dd654a03ad01937da
ebrandwein21 <150644846+ebrandwein21@users.noreply.github.com>
2025-04-02 19:45:51 -0500

    added the note ons and note offs for standard midi event

--- MyLittleMozart/src/edu/mu/absfactory/midievent/StandardMidiEventFactory.java
+++ MyLittleMozart/src/edu/mu/absfactory/midievent/StandardMidiEventFactory.java
@@ -1,5 +1,27 @@
 package edu.mu.absfactory.midievent;
 
-public class StandardMidiEventFactory {
+import javax.sound.midi.*;
+
+public class StandardMidiEventFactory implements MidiEventFactory{
+
+	@Override
+	public MidiEvent createNoteOn(int tick, int note, int velocity, int channel) throws InvalidMidiDataException {
+		// TODO Auto-generated method stub
+		
+		ShortMessage noteOnCreation = new ShortMessage();
+		noteOnCreation.setMessage(ShortMessage.NOTE_ON, 2, 2, 2); //dont know what to set values 
+				
+		return null;
+	}
+
+	@Override
+	public MidiEvent createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException {
+		ShortMessage noteOffCreation = new ShortMessage();
+		noteOffCreation.setMessage(ShortMessage.NOTE_OFF, 2, 2, 2);
+		
+		return null;
+	}
+	
+	
 
 }
