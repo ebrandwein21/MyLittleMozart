8bfb71287db5caf4d1973f9c7c41a7b81e6cb3e9
ebrandwein21 <150644846+ebrandwein21@users.noreply.github.com>
2025-04-03 14:52:11 -0500

    added csv main method and debugged csv parser

--- MyLittleMozart/src/edu/mu/mylittlemozart/Main.java
+++ MyLittleMozart/src/edu/mu/mylittlemozart/Main.java
@@ -1,10 +1,35 @@
 package edu.mu.mylittlemozart;
+import edu.mu.mylittlemozart.*;
+import edu.mu.mylittlemozart.csv.*;
+import edu.mu.mylittlemozart.csv.parsing.*;
+import edu.mu.mylittlemozart.absfactory.*;
+import edu.mu.mylittlemozart.absfactory.midievent.*;
+import edu.mu.mylittlemozart.instrument.*;
+import edu.mu.mylittlemozart.instrument.strategy.*;
+import java.util.List;
+import javax.sound.midi.*;
 
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	public static void main(String[] args)
+	{
+				try {
+					        List<MidiEventData> midiEvent = MidiCsvParser.midiEventListCreator("/Users/ethanbrandwien/git/MyLittleMozart/MyLittleMozart/src/edu/mu/mylittlemozart/mystery_song.csv");
+							Sequence sequence = new Sequence(Sequence.PPQ, 384);
+					        Track track = sequence.createTrack();
+					        System.out.println(midiEvent.get(4).getNote());
+				
+					        MidiEventFactoryAbstract factoryAbstract = 
+					        
+					        
+					        
+					}catch(Exception e)
+				{
+					e.printStackTrace();
+				}
 		
-	}	
-
+			
+	}
+	
+	
 }
---
+++ MyLittleMozart/src/edu/mu/mylittlemozart/csv/parsing/MidiCsvParser.java
@@ -0,0 +1,65 @@
+package edu.mu.mylittlemozart.csv.parsing;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MidiCsvParser{
+	
+	
+	static List<MidiEventData> midiEventDataList = new ArrayList<>(); 
+	
+    public static List<MidiEventData> midiEventListCreator(String filePath ) {
+    	
+	try(BufferedReader csvReader = new BufferedReader(new FileReader(filePath)))
+	{
+
+	String data=""; 
+	
+	while((data = csvReader.readLine()) != null) {
+		String[] objects = data.split(",");
+				
+		int startEndTick = Integer.parseInt(objects[0]);
+		
+		int noteOnOff = -1;
+		
+		if(objects[1].contains("Note_off_c"))
+		{
+			 noteOnOff = 0;
+		}
+		if(objects[1].contains("Note_on_c"))
+		{
+			 noteOnOff = 1;
+		}
+		
+        int velocity = Integer.parseInt(objects[2]);
+        int note = Integer.parseInt(objects[3]);
+        int channel = Integer.parseInt(objects[4]);
+        int instrument = Integer.parseInt(objects[5]);
+        
+       
+		MidiEventData midiEventData = new MidiEventData(startEndTick, noteOnOff, velocity, note, channel, instrument);
+		
+		//need to create instances where you would use the class and parse data from the constructor and add 
+		midiEventDataList.add(midiEventData);
+		
+		
+	}
+	
+	} catch(FileNotFoundException e) {
+		e.printStackTrace();
+	}
+	  catch(IOException error) {
+		  error.printStackTrace();
+    }
+	
+	return midiEventDataList;
+	//create a folder inside the folder with the csv 
+}
+
+
+}
+
