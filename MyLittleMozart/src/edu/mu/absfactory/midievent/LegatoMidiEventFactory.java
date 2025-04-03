9843413065f025bb726349140fb41e1b1e4155d1
ebrandwein21 <150644846+ebrandwein21@users.noreply.github.com>
2025-04-02 22:20:17 -0500

    added legato and staccato events

--- MyLittleMozart/src/edu/mu/absfactory/midievent/LegatoMidiEventFactory.java
+++ MyLittleMozart/src/edu/mu/absfactory/midievent/LegatoMidiEventFactory.java
@@ -1,5 +1,25 @@
 package edu.mu.absfactory.midievent;
 
-public class LegatoMidiEventFactory {
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.ShortMessage;
+
+public class LegatoMidiEventFactory implements MidiEventFactory {
+
+	@Override
+	public MidiEvent createNoteOn(int tick, int note, int velocity, int channel) throws InvalidMidiDataException {
+		int duration = tick + 80;
+		ShortMessage noteOnCreation = new ShortMessage();
+		noteOnCreation.setMessage(ShortMessage.NOTE_ON, channel, note, velocity);
+		return new MidiEvent(noteOnCreation, duration);
+	}
+
+	@Override
+	public MidiEvent createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException {
+		int duration = tick + 80;
+		ShortMessage noteOffreation = new ShortMessage();
+		noteOffreation.setMessage(ShortMessage.NOTE_ON, channel, note);
+		return new MidiEvent(noteOffreation, duration);
+	}
 
 }
--- MyLittleMozart/src/edu/mu/absfactory/midievent/StaccatoMidiEventFactory.java
+++ MyLittleMozart/src/edu/mu/absfactory/midievent/StaccatoMidiEventFactory.java
@@ -1,5 +1,30 @@
 package edu.mu.absfactory.midievent;
 
-public class StaccatoMidiEventFactory {
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.ShortMessage; 
+
+public class StaccatoMidiEventFactory implements MidiEventFactory{
+
+	@Override
+	public MidiEvent createNoteOn(int tick, int note, int velocity, int channel) throws InvalidMidiDataException {
+		// TODO Auto-generated method stub
+	int duration = tick - 120;
+	ShortMessage noteOnCreation = new ShortMessage();
+	noteOnCreation.setMessage(ShortMessage.NOTE_ON, channel, note, velocity);
+	return new MidiEvent(noteOnCreation, duration);
+
+	}
+
+	
+	@Override
+	public MidiEvent createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException {
+		// TODO Auto-generated method stub
+		int duration = tick - 120;
+		ShortMessage noteOffCreation = new ShortMessage();
+		noteOffCreation.setMessage(ShortMessage.NOTE_ON, channel, note);
+		return new MidiEvent(noteOffCreation, duration);
+
+	}
 
 }
