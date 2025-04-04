bf0db41b3ee16548b7b936a998f8d79b10c92092
andergato <145510193+andergato@users.noreply.github.com>
2025-04-03 20:07:18 -0500

    Merge

--- MyLittleMozart/src/edu/mu/mylittlemozart/instrument/strategy/InstrumentStrategy.java
+++ MyLittleMozart/src/edu/mu/mylittlemozart/instrument/strategy/InstrumentStrategy.java
@@ -2,5 +2,7 @@ package edu.mu.mylittlemozart.instrument.strategy;
 import javax.sound.midi.*;
 
 public interface InstrumentStrategy {
-	void applyInstrument(Track track, int channel);
+	void applyInstrument(Track track, int channel) throws MidiUnavailableException, InvalidMidiDataException;
 }
+
+
