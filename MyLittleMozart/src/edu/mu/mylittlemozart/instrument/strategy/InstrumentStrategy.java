2ac5c920fdb4430766dcfd3c326d207126a8974a
andergato <145510193+andergato@users.noreply.github.com>
2025-04-04 09:01:08 -0500

    Added JavaDoc for the Instrument Strategy and Pitch Strategy interfaces

--- MyLittleMozart/src/edu/mu/mylittlemozart/instrument/strategy/InstrumentStrategy.java
+++ MyLittleMozart/src/edu/mu/mylittlemozart/instrument/strategy/InstrumentStrategy.java
@@ -1,6 +1,10 @@
 package edu.mu.mylittlemozart.instrument.strategy;
 import javax.sound.midi.*;
 
+/*
+ * The Instrument Strategy interface allows for a dynamic change in instruments being used.
+ * It takes a Track and a channel integer as parameters and throws Midi Exceptions.
+ */
 public interface InstrumentStrategy {
 	void applyInstrument(Track track, int channel) throws MidiUnavailableException, InvalidMidiDataException;
 }
--- MyLittleMozart/src/edu/mu/mylittlemozart/pitch/strategy/PitchStrategy.java
+++ MyLittleMozart/src/edu/mu/mylittlemozart/pitch/strategy/PitchStrategy.java
@@ -1,5 +1,9 @@
 package edu.mu.mylittlemozart.pitch.strategy;
 
+/*
+ * The Pitch Strategy interface allows for a dynamic change in pitch.
+ * It takes a note as a parameter and strategies can be used to modify this note.
+ */
 public interface PitchStrategy {
 	int modifyPitch(int note);
 }
