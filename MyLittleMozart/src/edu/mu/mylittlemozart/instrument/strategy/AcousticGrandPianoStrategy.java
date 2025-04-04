bf2b3859c037c040263813c4ea54a4771459216b
andergato <145510193+andergato@users.noreply.github.com>
2025-04-04 08:06:08 -0500

    Commented out seemingly unneeded code in AcousticGrandPianoStrategy

--- MyLittleMozart/src/edu/mu/mylittlemozart/instrument/strategy/AcousticGrandPianoStrategy.java
+++ MyLittleMozart/src/edu/mu/mylittlemozart/instrument/strategy/AcousticGrandPianoStrategy.java
@@ -1,12 +1,12 @@
 package edu.mu.mylittlemozart.instrument.strategy;
 
-import javax.sound.midi.Instrument;
+//import javax.sound.midi.Instrument;
 import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiEvent;
-import javax.sound.midi.MidiSystem;
+//import javax.sound.midi.MidiSystem;
 import javax.sound.midi.MidiUnavailableException;
 import javax.sound.midi.ShortMessage;
-import javax.sound.midi.Synthesizer;
+//import javax.sound.midi.Synthesizer;
 import javax.sound.midi.Track;
 
 public class AcousticGrandPianoStrategy implements InstrumentStrategy {
@@ -15,14 +15,14 @@ public class AcousticGrandPianoStrategy implements InstrumentStrategy {
 	
 	@Override
 	public void applyInstrument(Track track, int channel) throws MidiUnavailableException, InvalidMidiDataException {
-		Synthesizer synth = MidiSystem.getSynthesizer(); 	
+//		Synthesizer synth = MidiSystem.getSynthesizer(); 	
 
 		ShortMessage sm = new ShortMessage( );
 		sm.setMessage(ShortMessage.PROGRAM_CHANGE, channel, instrument, 0); //9 ==> is the channel 10.
 		track.add(new MidiEvent(sm, 0));
 		
-		Instrument[] instr = synth.getDefaultSoundbank().getInstruments();
-		synth.loadInstrument(instr[instrument]);
+//		Instrument[] instr = synth.getDefaultSoundbank().getInstruments();
+//		synth.loadInstrument(instr[instrument]);
 		
 	}
 
